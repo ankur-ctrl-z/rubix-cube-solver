8445261cd43a168fe66c019e11d839dad1a8e001
ANKUR SHARMA <ankur143513@gmail.com>
2026-04-29 11:33:58 +0530

    update CORS for production

--- services/cv-service/main.py
+++ services/cv-service/main.py
@@ -12,7 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://rubix-cube-solver.vercel.app"
+        "https://rubix-cube-solver.vercel.app",
+        "https://*.vercel.app"
     ],
     allow_methods=["*"],
     allow_headers=["*"],
